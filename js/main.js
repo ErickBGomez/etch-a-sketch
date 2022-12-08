3e24d3589fe3729dae454f02fb37897d8bae9d1d
Erick B. Gómez <erickb.gomez28@gmail.com>
2022-12-08 10:34:31 -0600

    Add select tool function and default settings
    Now the user can select the desired tool, and their selection will be
    saved in a variable

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log("Hello world");
+// Default settings
+const DEFAULT_TOOL = "pencil";
+
+// Select nodes
+const allTools = document.querySelectorAll("button.tool-button");
+
+// Other variables
+let currentTool = null;
+
+// First loading settings
+selectTool(DEFAULT_TOOL);
+
+// Set Events
+allTools.forEach(tool => {
+    tool.addEventListener("click", selectTool);
+});
+
+// Functions
+function selectTool(newTool) {
+    // If a PointerEvent (object) is received, save only its ID string
+    // Otherwise, save just the string
+    const toolIdSelected = (typeof newTool === "object") ? newTool.target.id : newTool;
+
+    allTools.forEach(tool => {
+        setButtonState(tool, "unselected");
+
+        if (tool.id === toolIdSelected) {
+            setButtonState(tool, "selected");
+            currentTool = toolIdSelected;
+        }
+    });
+}
+
+function setButtonState(button, newState) {
+    button.dataset.state = newState;
+}
