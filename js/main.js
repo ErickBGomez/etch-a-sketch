1a0ba587502739b3a1dc881c3a677d67babdb2e8
Erick B. Gómez <erickb.gomez28@gmail.com>
2022-12-09 09:21:46 -0600

    Remove all pixels before setting new canvas size
    Each time the canvas will change its size, all pixels will be removed
    to avoid overloading the canvas' grid

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 // Default settings
 const DEFAULT_TOOL = "pencil";
+const DEFAULT_SIZE = 16;
 
 // Select nodes
 const allTools = document.querySelectorAll("button.tool-button");
+
+const canvas = document.querySelector("div#canvas");
+
+const showGridButton = document.querySelector("button#show-grid");
 
 // Other variables
 let currentTool = null;
 
 // First loading settings
+setCanvasSize(DEFAULT_SIZE);
 selectTool(DEFAULT_TOOL);
+setButtonState(showGridButton, "unselected");
+
+console.log(canvas.childElementCount);
 
 // Set Events
 allTools.forEach(tool => {
@@ -16,6 +25,31 @@ allTools.forEach(tool => {
 });
 
 // Functions
+
+function setCanvasSize(newSize) {
+    clearCanvas();
+
+    let newPixel = null;
+
+    canvas.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
+
+    for (let i = 0; i < newSize**2; i++) {
+        newPixel = document.createElement("div");
+        newPixel.classList.add("pixel");
+        newPixel.classList.add("pixel-border");
+
+        canvas.appendChild(newPixel);
+    }
+}
+
+function clearCanvas() {
+    if (canvas.hasChildNodes()) {
+        // Remove all pixels
+        canvas.innerHTML = "";
+    }
+}
+
 function selectTool(newTool) {
     // If a PointerEvent (object) is received, save only its ID string
     // Otherwise, save just the string
